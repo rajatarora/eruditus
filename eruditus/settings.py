fde7a8d9568f2fc091d763c9bc6d33df295bebf3
Rajat Arora <rajat@rajatarora.info>
2013-04-24 22:00:58 +0530

    Minor edits to models

--- eruditus/settings.py
+++ eruditus/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'eruditus_user',
         'PASSWORD': 'eruditus123',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
--- eruditus_app/models.py
+++ eruditus_app/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
     sites = models.ManyToManyField(Site)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField
+    email = models.EmailField()
     
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
@@ -34,7 +34,7 @@ class Term(models.Model):
 class License(models.Model):
     short_name = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=50)
-    url = models.URLField
+    url = models.URLField()
     
     def __unicode__(self):
         return self.name
@@ -42,18 +42,15 @@ class License(models.Model):
 
 class Post(models.Model):
     user = models.ForeignKey(User)
-    publish_date = models.DateTimeField
+    publish_date = models.DateTimeField()
     n_comment = models.IntegerField(default=0)
     terms = models.ManyToManyField(Term)
-    
-    class Meta:
-        abstract = True
 
    
 class Text(Post):
     title = models.CharField(max_length=500)
     subtitle = models.CharField(max_length=500)
-    content = models.TextField
+    content = models.TextField()
 
     def __unicode__(self):
         return self.title
@@ -64,16 +61,16 @@ class Image(Post):
     image = models.FileField(upload_to='images')
     license = models.ForeignKey(License)
     source_name = models.CharField(max_length=30)
-    source_url = models.URLField
+    source_url = models.URLField()
     
     def __unicode__(self):
         return self.title
 
     
 class Link(Post):
-    link = models.URLField
+    link = models.URLField()
     link_text = models.CharField(max_length=500)
-    description = models.TextField
+    description = models.TextField()
     
     def __unicode__(self):
         return self.link
@@ -81,7 +78,7 @@ class Link(Post):
 
 class Aside(Post):
     title = models.CharField(max_length=500)
-    content = models.TextField
+    content = models.TextField()
     
     def __unicode__(self):
         return self.title
@@ -90,7 +87,7 @@ class Aside(Post):
 class Quote(Post):
     quote = models.CharField(max_length=1000)
     by = models.CharField(max_length=30)
-    by_url = models.URLField
+    by_url = models.URLField()
     
     def __unicode__(self):
         return self.quote
@@ -99,9 +96,9 @@ class Quote(Post):
 class Comment(models.Model):
     post = models.ForeignKey(Post)
     author = models.CharField(max_length=30)
-    author_email = models.EmailField
-    author_url = models.URLField
-    text = models.TextField
+    author_email = models.EmailField()
+    author_url = models.URLField()
+    text = models.TextField()
     
     def __unicode(self):
         return self.text
